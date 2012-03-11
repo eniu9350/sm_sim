6bcfbbf55ee04f423f702efe882f5dd658551bdb
eniu9350 <niujin@gmail.com>
2012-03-11 19:04:29 +0800

    client

--- sm_client.h
+++ sm_client.h
@@ -1,14 +1,9 @@
 #ifndef _SM_SIM_SM_CLIENT_
 #define _SM_SIM_SM_CLIENT_
 
-#include "common.h"
+
 #include "ch.h"
 
-#define ET_CLIENT_HB_REQ 0x10
-#define ET_CLIENT_HB_RSP 0x11
-#define ET_CLIENT_BC_REQ 0x12
-#define ET_CLIENT_BC_RSP 0x13
-#define ET_CLIENT_CHCHANGE 0x14
 //#define ET_CLIENT_SRV_REQ 4
 
 
@@ -35,10 +30,10 @@ typedef struct channel_info_client	{
 }channel_info_client;
 
 
-typedef struct client	{
+typedef struct sm_client	{
 	int id;
 
-	context_global* gctx;
+	//context_global* gctx;
 
 	int ci_update_mode;
 	channel_info_client* ci;
@@ -47,7 +42,7 @@ typedef struct client	{
 
 	int state;
 
-}client;
+}sm_client;
 
 
 /*------- channel ops--------------------------*/
